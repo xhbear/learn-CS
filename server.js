521668f56f9707ecc1247a0796180a6125aac2b2
xhbear <xionghsinh106888@gmail.com>
2021-09-03 12:52:28 +0800

    Dir fix
    Re-arranged directory and in-line references.

--- server.js
+++ server.js
@@ -2,25 +2,32 @@ const express = require('express')
 const app = express()
 const path= require('path')
 const router = express.Router();
+const bcrypt = require('bcrypt')
 
-app.use(express.urlencoded({ extended: false }))
-app.use(express.static("home ui"));
+app.use(express.urlencoded({ extended: false}))
+
+app.use(express.static(__dirname + 'public'));
+//app.use(express.static(__dirname + '/productivity'));
+//app.use(express.static(__dirname + '/login'));
+//app.use(express.static(__dirname + '/register'));
+
+//app.use('/LEARN-CS', express.static('home'));
 
 router.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/home ui/index.html'));
+    res.sendFile(path.join(__dirname + '/views/home.html'));
     //__dirname: It will resolve to project folder
 });
 
-router.get('/main', function (req, res) {
-    res.sendFile(path.join(__dirname + '/to-do-list ui/main.html'));
+router.get('/productivity', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/productivity.html'));
 });
 
 router.get('/login', function (req, res) {
-    res.sendFile(path.join(__dirname + '/login ui/login.html'));
+    res.sendFile(path.join(__dirname + '/views/login.html'));
 });
 
 router.get('/register', function (req, res) {
-    res.sendFile(path.join(__dirname + '/register ui/register.html'));
+    res.sendFile(path.join(__dirname + '/views/register.html'));
 });
 
 //add router
--- views/scripts/home-scripts.js
+++ views/scripts/home-scripts.js
@@ -9,14 +9,6 @@ const navSlide= () => {
         //Toggle Nav
         nav.classList.toggle('nav-active');
 
-        //Animate Lines
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = '';
-            } else {
-                link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s';
-            }
-        });
         //Burger Animation
         burger.classList.toggle('toggle');   
     });
