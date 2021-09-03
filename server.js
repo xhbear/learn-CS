7999c1ef0d1c6ba8bc69617e1ca5875fc4891b1a
xhbear <xionghsinh106888@gmail.com>
2021-09-03 15:49:26 +0800

    Fixed webpage interaction
    Persisting BUG: css styling not applying to webpages.

--- server.js
+++ server.js
@@ -1,37 +1,93 @@
-const express = require('express')
-const app = express()
-const path= require('path')
-const router = express.Router();
-const bcrypt = require('bcrypt')
+if (process.env.NODE_ENV !== 'production') {
+    require ('dotenv').config()
+}
 
+const express = require('express');
+const app = express();
+const bcrypt = require('bcrypt');
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+const methodOVerride = require('method-override')
+
+const initializePassport = require('./passport-config')
+initializePassport(
+    passport, 
+    email => users.find(user => user.email === email),
+    id => users.find(user => user.id === id)
+) 
+
+const users = []
+
+app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false}))
+app.use(flash())
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOVerride('_method'))
+app.use('/public', express.static('public'))
 
-app.use(express.static(__dirname + 'public'));
-//app.use(express.static(__dirname + '/productivity'));
-//app.use(express.static(__dirname + '/login'));
-//app.use(express.static(__dirname + '/register'));
+app.get('/', checkAuthenticated, (req, res) => {
+    res.render('home.ejs', { name: req.user.name })
+})
 
-//app.use('/LEARN-CS', express.static('home'));
+app.get('/productivity', checkAuthenticated, (req, res) => {
+    res.render('productivity.ejs')
+})
 
-router.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/views/home.html'));
-    //__dirname: It will resolve to project folder
-});
+app.get('/login', checkNotAuthenticated, (req, res) => {
+    res.render('login.ejs')
+})
 
-router.get('/productivity', function (req, res) {
-    res.sendFile(path.join(__dirname + '/views/productivity.html'));
-});
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}))
 
-router.get('/login', function (req, res) {
-    res.sendFile(path.join(__dirname + '/views/login.html'));
-});
+app.get('/register', checkNotAuthenticated, (req, res) => {
+    res.render('register.ejs')
+})
 
-router.get('/register', function (req, res) {
-    res.sendFile(path.join(__dirname + '/views/register.html'));
-});
+//register user
+app.post('/register', checkNotAuthenticated, async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        })
+        res.redirect('/login')
+    } catch {
+        res.redirect('/register')
+    }
+})
 
-//add router
-app.use('/', router);
+app.delete('/logout', (req, res) => {
+    req.logOut()
+    res.redirect('/login')
+})
+
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next()
+    }
+
+    res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return res.redirect('/')
+    }
+    next()
+}
+
 app.listen(process.env.port || 3000);
-
-console.log('Running at Port 3000');
